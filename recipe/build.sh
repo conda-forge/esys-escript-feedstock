ce6766bfa366645e16115b70f50974f8e563ba7e
esys-escript <a.ellery@uq.edu.au>
2020-12-07 13:48:28 +1000

    Update build.sh
    bug fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,23 +6,22 @@ set -o pipefail
 CFLAGS="${CFLAGS} -I${PREFIX}/include -fPIC"
 CXXFLAGS="${CXXFLAGS} -fPIC -w -fopenmp"
 
-DEFAULT_HDF5_INCDIR=$PREFIX/include
-DEFAULT_HDF5_LIBDIR=$PREFIX/lib
-cd ${SRC_DIR}/silo
-./configure --prefix=${PREFIX} \
-        --with-hdf5=${PREFIX}/include,${PREFIX}/lib \
-        --with-zlib=$PREFIX/include,$PREFIX/lib
-make -j"${CPU_COUNT}"
-make -j"${CPU_COUNT}" install
-
 if [ ${CONDA_PY} -eq 38 ]
 then
     BOOST_LIBS="boost_python${CONDA_PY}"
     PYTHON_LIB_PATH="${PREFIX}/lib"
     PYTHON_INC_PATH="${PREFIX}/include/python${PY_VER}"
     PYTHON_LIB_NAME="python${PY_VER}"
-    BUILD_SILO=1
+    BUILD_SILO=0
 else
+    DEFAULT_HDF5_INCDIR=$PREFIX/include
+    DEFAULT_HDF5_LIBDIR=$PREFIX/lib
+    cd ${SRC_DIR}/silo
+    ./configure --prefix=${PREFIX} \
+            --with-hdf5=${PREFIX}/include,${PREFIX}/lib \
+            --with-zlib=$PREFIX/include,$PREFIX/lib
+    make -j"${CPU_COUNT}"
+    make -j"${CPU_COUNT}" install
     BOOST_LIBS="boost_python${CONDA_PY}"
     PYTHON_LIB_PATH="${PREFIX}/lib"
     PYTHON_INC_PATH="${PREFIX}/include/python${PY_VER}m"
