4e97b2d1b79b7477ac9c89763ead1ddb301f5d1a
esys-escript <a.ellery@uq.edu.au>
2020-12-18 16:54:59 +1000

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -17,10 +17,31 @@ if errorlevel 1 exit \b 1
 cmake --build . --config %NC_BUILD_TYPE% --target install
 if errorlevel 1 exit \b 1
 
+:: now build silo
+::
+set SILO_BUILD_TYPE=Release
+cd %SRC_DIR%\silo\SiloWindows\MSVC2012
+msbuild Silo.sln ^
+    -toolsVersion:15.0 ^
+    -property:Configuration=%SILO_BUILD_TYPE% ^
+    -property:PlatformToolset=v141;WindowsTargetPlatformVersion=%WindowsSDKVer% ^
+    -property:HDF5_INC_DIR_X64=%LIBRARY_INC% ^
+    -property:HDF5_LIB_DIR_X64=%LIBRARY_LIB%
+if errorlevel 1 exit \b 1
+echo Install configuration: "%SILO_BUILD_TYPE%"
+copy /y %SRC_DIR%\silo\SiloWindows\include\silo*.h "%LIBRARY_INC%"
+if errorlevel 1 exit \b 1
+copy /y %SRC_DIR%\silo\src\silo\silo_exports.h "%LIBRARY_INC%"
+if errorlevel 1 exit \b 1
+copy /y %SRC_DIR%\silo\SiloWindows\MSVC2012\x64\%SILO_BUILD_TYPE%\silohdf5.lib "%LIBRARY_LIB%"
+if errorlevel 1 exit \b 1
+copy /y %SRC_DIR%\silo\SiloWindows\MSVC2012\x64\%SILO_BUILD_TYPE%\silohdf5.dll "%LIBRARY_BIN%"
+if errorlevel 1 exit \b 1
+
 :: workaround for bug in win-64/boost-1.73.0-py38_11
-if not exist "%LIBRARY_PREFIX%\include\boost\python.hpp" (
-    if exist "%LIBRARY_PREFIX%\include\boost\python\python.hpp" (
-        copy "%LIBRARY_PREFIX%\include\boost\python\python.hpp" "%LIBRARY_PREFIX%\include\boost"
+if not exist "%LIBRARY_INC%\boost\python.hpp" (
+    if exist "%LIBRARY_INC%\boost\python\python.hpp" (
+        copy "%LIBRARY_INC%\boost\python\python.hpp" "%LIBRARY_INC%\boost"
     )
 )
 :: now build escript
@@ -29,6 +50,7 @@ cd %SRC_DIR%\escript
 call scons -j%CPU_COUNT% ^
     options_file="scons\templates\windows_msvc141_options.py" ^
     compressed_files=0 ^
+    silo=1 ^
     prefix="%PREFIX%" ^
     build_dir="%BUILD_PREFIX%\escript_build" ^
     build_full
