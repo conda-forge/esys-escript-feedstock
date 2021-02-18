b49644609a0595711e0e368b233cda399f2784af
esys-escript <a.ellery@uq.edu.au>
2021-02-18 11:30:31 +1000

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -18,9 +18,9 @@ cmake --build . --config %NC_BUILD_TYPE% --target install
 if errorlevel 1 exit \b 1
 
 :: workaround for bug in win-64/boost-1.73.0-py38_11
-if not exist "%LIBRARY_INC%\boost\python.hpp" (
-    if exist "%LIBRARY_INC%\boost\python\python.hpp" (
-        copy "%LIBRARY_INC%\boost\python\python.hpp" "%LIBRARY_INC%\boost"
+if not exist "%LIBRARY_PREFIX%\include\boost\python.hpp" (
+    if exist "%LIBRARY_PREFIX%\include\boost\python\python.hpp" (
+        copy "%LIBRARY_PREFIX%\include\boost\python\python.hpp" "%LIBRARY_PREFIX%\include\boost"
     )
 )
 :: now build escript
@@ -29,7 +29,6 @@ cd %SRC_DIR%\escript
 call scons -j%CPU_COUNT% ^
     options_file="scons\templates\windows_msvc141_options.py" ^
     compressed_files=0 ^
-    silo=0 ^
     prefix="%PREFIX%" ^
     build_dir="%BUILD_PREFIX%\escript_build" ^
     build_full
@@ -39,4 +38,3 @@ if errorlevel 1 exit \b 1
 copy /y %SRC_DIR%\escript\LICENSE %SRC_DIR%\LICENSE
 copy /y %PREFIX%\esys %SP_DIR%\esys
 copy /y %BUILD_PREFIX%\escript_build\scripts\release_sanity.py %TEMP%\release_sanity.py
-
