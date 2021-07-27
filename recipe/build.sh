17d81f387bea42a018b3da6e20af1f623860dec7
esys-escript <a.ellery@uq.edu.au>
2021-07-27 11:06:23 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,14 +14,14 @@ then
     PYTHON_LIB_NAME="python${PY_VER}"
     BUILD_SILO=0
 else
-    DEFAULT_HDF5_INCDIR=$PREFIX/include
-    DEFAULT_HDF5_LIBDIR=$PREFIX/lib
-    cd ${SRC_DIR}/silo
-    ./configure --prefix=${PREFIX} \
-            --with-hdf5=${PREFIX}/include,${PREFIX}/lib \
-            --with-zlib=$PREFIX/include,$PREFIX/lib
-    make -j"${CPU_COUNT}"
-    make -j"${CPU_COUNT}" install
+#     DEFAULT_HDF5_INCDIR=$PREFIX/include
+#     DEFAULT_HDF5_LIBDIR=$PREFIX/lib
+#     cd ${SRC_DIR}/silo
+#     ./configure --prefix=${PREFIX} \
+#             --with-hdf5=${PREFIX}/include,${PREFIX}/lib \
+#             --with-zlib=$PREFIX/include,$PREFIX/lib
+#     make -j"${CPU_COUNT}"
+#     make -j"${CPU_COUNT}" install
     BOOST_LIBS="boost_python${CONDA_PY}"
     PYTHON_LIB_PATH="${PREFIX}/lib"
     PYTHON_INC_PATH="${PREFIX}/include/python${PY_VER}m"
