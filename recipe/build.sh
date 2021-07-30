c02fa06d64f3e2839de61453745298693a71e668
esys-escript <a.ellery@uq.edu.au>
2021-07-30 22:07:26 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,14 +6,35 @@ set -o pipefail
 CFLAGS="${CFLAGS} -I${PREFIX}/include -fPIC"
 CXXFLAGS="${CXXFLAGS} -fPIC -w -fopenmp"
 
-BOOST_LIBS="boost_python${CONDA_PY}"
-PYTHON_LIB_PATH="${PREFIX}/lib"
-PYTHON_INC_PATH="${PREFIX}/include/python${PY_VER}"
-PYTHON_LIB_NAME="python${PY_VER}"
-
+if [ ${CONDA_PY} -eq 39 ]
+then
+    BOOST_LIBS="boost_python${CONDA_PY}"
+    PYTHON_LIB_PATH="${PREFIX}/lib"
+    PYTHON_INC_PATH="${PREFIX}/include/python${PY_VER}"
+    PYTHON_LIB_NAME="python${PY_VER}"
+elif [ ${CONDA_PY} -eq 38 ]
+then
+    BOOST_LIBS="boost_python${CONDA_PY}"
+    PYTHON_LIB_PATH="${PREFIX}/lib"
+    PYTHON_INC_PATH="${PREFIX}/include/python${PY_VER}"
+    PYTHON_LIB_NAME="python${PY_VER}"
+elif [ ${CONDA_PY} -eq 37 ]
+then
+    BOOST_LIBS="boost_python${CONDA_PY}"
+    PYTHON_LIB_PATH="${PREFIX}/lib"
+    PYTHON_INC_PATH="${PREFIX}/include/python${PY_VER}m"
+    PYTHON_LIB_NAME="python${PY_VER}m"
+elif [ ${CONDA_PY} -eq 36 ]
+then
+    BOOST_LIBS="boost_python${CONDA_PY}"
+    PYTHON_LIB_PATH="${PREFIX}/lib"
+    PYTHON_INC_PATH="${PREFIX}/include/python${PY_VER}m"
+    PYTHON_LIB_NAME="python${PY_VER}m"
+fi
 BUILD_SILO=0
 
 cd ${SRC_DIR}/escript
+
 scons -j"${CPU_COUNT}" \
     options_file="${SRC_DIR}/escript/scons/templates/anaconda_python3_options.py" \
     build_dir=${BUILD_PREFIX}/escript_build \
@@ -31,6 +52,7 @@ scons -j"${CPU_COUNT}" \
     pythonlibname=${PYTHON_LIB_NAME} \
     silo=${BUILD_SILO} \
     silo_prefix=${PREFIX} \
+    umfpack=1 \
     umfpack_prefix=${PREFIX} \
     build_full || cat config.log
 
